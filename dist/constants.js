b08eae2c7f4868947a95b3baccc336de77e9ebaa
omekrit <khomkrit.y@gmail.com>
2021-06-24 14:21:26 +0700

    add basic post crud functionalities

--- dist/constants.js
+++ dist/constants.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CONFIG = void 0;
 exports.CONFIG = {
     PROD: process.env.NODE_ENV === 'production',
-    PASSWORD: 'omekrit123'
+    PASSWORD: 'omekrit123',
+    PORT: 5000
 };
 //# sourceMappingURL=constants.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
 import { CONFIG } from './constants';
@@ -5,6 +6,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   // ts orm setup with postgresql
@@ -17,9 +19,10 @@ const main = async () => {
   // graphql schema
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   // graphql endpoint

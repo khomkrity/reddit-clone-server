f9c0d12c904fdb069577660ad5784e4f4ac19376
omekrit <khomkrit.y@gmail.com>
2021-06-24 13:26:16 +0700

    initially setup express, apollo, graphql

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const CONFIG = {
     PROD: process.env.NODE_ENV === 'production',
-    PASSWORD: 'omekrit123'
+    PASSWORD: 'omekrit123',
+    PORT: 5000
 };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import { CONFIG } from './constants';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
+  // ts orm setup with postgresql
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-    //   const post = orm.em.create(Post, { title: 'my first post' });
-    //   await orm.em.persistAndFlush(post);
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+  // restapi server
+  const app = express();
 
+  // graphql schema
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  // graphql endpoint
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(CONFIG.PORT, () => {
+    console.log(`server started on localhost:${CONFIG.PORT}`);
+  });
 };
 
 main().catch(err => {
